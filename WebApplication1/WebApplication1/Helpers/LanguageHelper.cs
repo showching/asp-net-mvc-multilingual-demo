10c330a56855f0919999f0079827acdbcc221868
showching <tong.xiaocheng@live.com>
2016-02-01 10:29:26 +0800

    add source code

---
+++ WebApplication1/WebApplication1/Helpers/LanguageHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Helpers
+{
+    public class LanguageHelper
+    {
+        public List<Language> GetLanguages()
+        {
+            List<Language> languages = new List<Language>
+            {
+                new Language{ID=1,Name="English",Code="en"},
+                new Language{ID=2,Name="Chinese",Code="zh"}
+            };
+
+            return languages;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Language.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Language
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+    }
+}
